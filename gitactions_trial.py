907246941c5555869e3a2a32bcd45e6311272891
Michael Ikebudu <michael.ikebudu@mycit.ie>
2020-12-01 16:26:05 +0000

    updated github actions

--- gitactions_trial.py
+++ gitactions_trial.py
@@ -1,3 +1,4 @@
+import datetime
 
 print("Hi")
 
@@ -6,11 +7,27 @@ def printhello():
     return "Hello_world"
 
 
-
-
-
 print("Michael")
+
 
 def printWord(word):
     return word
 
+
+print("Hi")
+
+
+def trial():
+    print("Hello world!")
+    word = "Hello world!"
+    now = datetime.datetime.now()
+    print("Current date and time is ")
+    d = "Current date and time is "
+    print(now.strftime("%A, %d-%m-%Y : %H:%M"))
+    t = now.strftime("%A, %d-%m-%Y : %H:%M")
+    return word, d, t
+
+
+print("Michael")
+print("David")
+print(trial())
--- test_utils.py
+++ test_utils.py
@@ -1,6 +1,17 @@
 import gitactions_trial
+
+
 def test_printhello():
-    assert gitactions_trial.printhello() =="Hello_world"
+    assert gitactions_trial.printhello() == "Hello_world"
+
 
 def test_printWord():
     assert gitactions_trial.printWord("Potat") == "Potato"
+
+    # Can't use pytest here because the time will change every second so it will always fail. So
+    # I added the python file to gitactions after running the pytest is passed. I can make the pytest work here
+    # but I have to use the now() function in the pytest instead of particular time in which the code was implemented.
+    # But I don't need to since github actions is also checking the main file for errors.
+    # def test_trial():
+    #     test = ('Hello world!', 'Current date and time is ', 'Monday, 30-11-2020 : 14:53')
+    #     assert trial() == test
